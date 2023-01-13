d7e4630baee77d7fb3ac5000f80116d640c9fba6
Denis Kuznetsov workPC <denis.kyznecov@gmail.com>
2023-01-13 15:46:53 +0300

    end of the day commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -20,26 +20,38 @@ class Board extends React.Component {
     );
   }
 
+  createBoard() {
+    let container = React.createElement("div");
+    let boardRow = React.createElement("div", {
+      className: "my-component",
+    });
+    let square = this.renderSquare(1);
+    console.log(boardRow, square);
+    boardRow.append(square);
+    container.append(boardRow);
+    return boardRow;
+  }
+
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    ;
+    
+    // <div>
+    //   <div className="board-row">
+    //     {this.renderSquare(0)}
+    //     {this.renderSquare(1)}
+    //     {this.renderSquare(2)}
+    //   </div>
+    //   <div className="board-row">
+    //     {this.renderSquare(3)}
+    //     {this.renderSquare(4)}
+    //     {this.renderSquare(5)}
+    //   </div>
+    //   <div className="board-row">
+    //     {this.renderSquare(6)}
+    //     {this.renderSquare(7)}
+    //     {this.renderSquare(8)}
+    //   </div>
+    // </div>
   }
 }
 
